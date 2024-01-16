4b66ff0bc3435c93364b9b7106af40524e418821
Radenko Vucanovic <radenkovuc@gmail.com>
2024-01-16 10:47:08 +0100

    Update ApiServices.ts

--- src/services/ApiServices.ts
+++ src/services/ApiServices.ts
@@ -1,11 +1,9 @@
-import axios from "axios";
-
 interface GetDataProps {
     url: string,
     params?: any
 }
 
 export const getData = async <T>({params, url}: GetDataProps): Promise<T[]> => {
-    const result = await axios.get<T[]>(`${process.env.URL || process.env.VERCEL_URL}/api/${url}`, {params});
-    return result.data
+    const result = await fetch(`${process.env.URL || process.env.VERCEL_URL}/api/${url}`);
+    return await result.json()
 }
