46d8a91914e943c873d7de784f35793422401c42
Radenko Vucanovic <radenkovuc@gmail.com>
2024-01-18 23:07:53 +0100

    Update route.ts

--- app/api/locations/sync/route.ts
+++ app/api/locations/sync/route.ts
@@ -1,11 +1,16 @@
 import {NextResponse} from "next/server";
 import {getLocationData, getLocations} from "@/src/services/dbServices";
+import {Data} from "@/src/domain/data";
 
 export async function GET() {
+    const allData: Data[][] = []
     const locations = await getLocations()
-    locations.forEach(location => getLocationData(location))
+    for (const location of locations) {
+        const data = await getLocationData(location)
+        allData.push(data)
+    }
 
-    return NextResponse.json({message: "SYNC DONE"}, {status: 200})
+    return NextResponse.json({message: "SYNC DONE", allData}, {status: 200})
 }
 
 export const dynamic = 'force-dynamic'
