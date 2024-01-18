6a7ccdd71a83608c8c9959561c2ed8f954536084
Radenko Vucanovic <radenkovuc@gmail.com>
2024-01-18 16:13:13 +0100

    added cron job

---
+++ app/api/locations/sync/route.ts
@@ -0,0 +1,9 @@
+import {NextResponse} from "next/server";
+import {getLocationData, getLocations} from "@/src/services/dbServices";
+
+export async function GET() {
+    const locations = await getLocations()
+    locations.forEach(location => getLocationData(location))
+
+    return NextResponse.json({message: "SYNC DONE"}, {status: 200})
+}
